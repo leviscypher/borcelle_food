aa485de275f9a260e1bf0fa5563bd107bde0076d
quangthang-web <taquangthang2002hn@gmail.com>
2023-01-30 14:11:56 +0700

    push mới

---
+++ social_network/src/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "SocialNetworkDefault",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
