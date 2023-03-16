94a5b484a60e57ba38ece1d1a890e2f30d4f0123
hoangtu1512002 <hoangtu1512002@gmail.com>
2023-03-16 08:33:36 +0700

    router

--- Back-End/borcelle_food/routes/api.php
+++ Back-End/borcelle_food/routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// admin api
+Route::prefix('admin')->group(function () {
+    Route::get('/test', function () {
+        return 'hello';
+    });
 });
+
+
+// customer api
